6708c1e5e082f7606ede36a5b0f3a3ddd6ad7533
Chidozie Ugwu <jonasbraham@gmail.com>
2024-06-19 21:03:07 +0100

    post tagging done

--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -1,1 +1,23 @@
-<div class="text-sm">Posted {{ $post->date->diffForHumans() }} by <a class="no-underline text-indigo-500 hover:text-indigo-700 transition-colors durration-100" href="/authors/{{ $post->author }}">{{ $post->author_name }}</a></div>
+<diiv class="flex items-center justify-between">
+
+    <div class="text-sm">Posted {{ $post->date->diffForHumans() }} by <a class="no-underline text-indigo-500 hover:text-indigo-700 transition-colors durration-100" href="/authors/{{ $post->author }}">{{ $post->author_name }}</a></div>
+    
+    @if (count($post->tags))
+    
+        <div class="flex flex-row items-center">
+    
+            @foreach ($post->tags as $tag)
+    
+                <a class="hover:opacity-75 transition-opacity duration-100 ease-in" href="/tags/{{ $tag }}">
+                    
+                    <span class="bg-indigo-500 text-white capitalize inline-flex items-center px-3 py-0.5 rounded-full text-xs font-semibold leading-5">{{ $tag }}</span>
+                
+                </a>
+    
+            @endforeach
+    
+        </div>
+    
+    @endif
+
+</diiv>
